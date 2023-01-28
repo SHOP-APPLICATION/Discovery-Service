c8de3270ab9df31d7b2ef12b8930118bd3190502
ACHAGHOUR ZAKARIA <achaghourzakaria@gmail.com>
2023-01-28 16:23:14 +0100

    WIP: Discovery service

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package config;
+
+
+//@Configuration
+public class SecurityConfig {
+/*
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
+   // @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }*/
+}
